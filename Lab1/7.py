a177eef6497e4e136efba5f7a4b35f8d313eccc1
dddYdd <74626121+dddYdd@users.noreply.github.com>
2025-02-08 13:05:34 +0500

    Add files via upload

---
+++ Lab1/7.py
@@ -0,0 +1,37 @@
+a = "Hello"
+print(a)
+#2
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+#3
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+#4
+b = "Hello, World!"
+print(b[2:5])
+#5
+a = "Hello, World!"
+print(a.upper())
+#6
+a = "Hello, World!"
+print(a.replace("H", "J"))
+#7
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+#8
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+#9
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+#10 The escape character allows you to use double quotes when you normally would not be allowed:
+txt = "We are the so-called \"Vikings\" from the north."
+print(txt)
+#11
