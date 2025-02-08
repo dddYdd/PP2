1caf847850c0e887a433922ee60196438e274d9a
dddYdd <74626121+dddYdd@users.noreply.github.com>
2025-02-08 15:31:24 +0500

    Update 2.4.py

--- Lab2/2.4.py
+++ Lab2/2.4.py
@@ -1,10 +1,4 @@
-# Tuples are used to store multiple items in a single variable.
 
-# Tuple is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Set, and Dictionary, all with different qualities and usage.
-
-#A tuple is a collection which is ordered and unchangeable.
-
-#Tuples are written with round brackets.
 #1
 thistuple = ("apple", "banana", "cherry")
 print(thistuple)
@@ -12,16 +6,13 @@ print(thistuple)
 thistuple = ("apple",)
 print(type(thistuple))
 
-#NOT a tuple
+
 thistuple = ("apple")
 print(type(thistuple))
-#3 Print the second item in the tuple:
+#3
 thistuple = ("apple", "banana", "cherry")
 print(thistuple[1])
-#4 Change Tuple Values
-#Once a tuple is created, you cannot change its values. Tuples are unchangeable, or immutable as it also is called.
-
-#But there is a workaround. You can convert the tuple into a list, change the list, and convert the list back into a tuple.
+#4 
 x = ("apple", "banana", "cherry")
 y = list(x)
 y[1] = "kiwi"
@@ -56,7 +47,7 @@ fruits = ("apple", "banana", "cherry")
 print(green)
 print(yellow)
 print(red)
-#2 Assign the rest of the values as a list called "red":
+#2 
 fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
 
 (green, yellow, *red) = fruits
