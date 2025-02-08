7a0a2ba7e7930e0876b88690eab270f1b78ea55e
dddYdd <74626121+dddYdd@users.noreply.github.com>
2025-02-08 15:30:37 +0500

    Update 2.3.py

--- Lab2/2.3.py
+++ Lab2/2.3.py
@@ -5,20 +5,20 @@ print(thisset)
 thisset = {"apple", "banana", "cherry"}
 
 print("banana" in thisset)
-#3 Once a set is created, you cannot change its items, but you can add new items.
+#3
 thisset = {"apple", "banana", "cherry"}
 
 thisset.add("orange")
 
 print(thisset)
-#4 To add items from another set into the current set, use the update() method.
+#4 
 thisset = {"apple", "banana", "cherry"}
 tropical = {"pineapple", "mango", "papaya"}
 
 thisset.update(tropical)
 
 print(thisset)
-#5 The object in the update() method does not have to be a set, it can be any iterable object (tuples, lists, dictionaries etc.).
+#5 
 thisset = {"apple", "banana", "cherry"}
 mylist = ["kiwi", "orange"]
 
@@ -51,7 +51,7 @@ thisset = {"apple", "banana", "cherry"}
 thisset.clear()
 
 print(thisset)
-#10 The del keyword will delete the set completely:
+#10 
 thisset = {"apple", "banana", "cherry"}
 
 del thisset
@@ -69,21 +69,21 @@ set2 = {1, 2, 3}
 
 set3 = set1 | set2
 print(set3)
-# The update() method inserts all items from one set into another.
+# 
 
-#The update() changes the original set, and does not return a new set.
+#
 set1 = {"a", "b" , "c"}
 set2 = {1, 2, 3}
 
 set1.update(set2)
 print(set1)
-# The intersection() method will return a new set, that only contains the items that are present in both sets.
+# 
 set1 = {"apple", "banana", "cherry"}
 set2 = {"google", "microsoft", "apple"}
 
 set3 = set1.intersection(set2)
 print(set3)
-# You can use the & operator instead of the intersection() method, and you will get the same result.
+# 
 set1 = {"apple", "banana", "cherry"}
 set2 = {"google", "microsoft", "apple"}
 
