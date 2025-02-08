0fe7e0cc769167a5fa24966fda40199512d334bf
dddYdd <74626121+dddYdd@users.noreply.github.com>
2025-02-08 15:33:16 +0500

    Update 2.7.py

--- Lab2/2.7.py
+++ Lab2/2.7.py
@@ -1,26 +1,26 @@
 #1 
 thislist = ["apple", "banana", "cherry"]
 print(thislist)
-#2 Print the number of items in the list:
+#2 
 thislist = ["apple", "banana", "cherry"]
 print(len(thislist))
-#3 Using the list() constructor to make a List:
-thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+#3 
+thislist = list(("apple", "banana", "cherry")) # 
 print(thislist)
-#4 Print the second item of the list:
+#4
 thislist = ["apple", "banana", "cherry"]
 print(thislist[1])
-#5 Add elements of a tuple to a list:
+#5
 thislist = ["apple", "banana", "cherry"]
 thistuple = ("kiwi", "orange")
 thislist.extend(thistuple)
 print(thislist)
-#6 If there are more than one item with the specified value, the remove() method removes the first occurrence:
+#6 
 thislist = ["apple", "banana", "cherry"]
 thislist.remove("banana")
 print(thislist)
-#7 The pop() method removes the specified index. 
-# If you do not specify the index, the pop() method removes the last item.
+#7 
+#
 thislist = ["apple", "banana", "cherry"]
 thislist.pop(1)
 print(thislist)
@@ -66,36 +66,34 @@ fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
 newlist = ['hello' for x in fruits]
 
 print(newlist)
-# sort lists
-#List objects have a sort() method that will sort the list alphanumerically, ascending, by default:
+# 
 thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
 thislist.sort()
 print(thislist)
-#2 Sort the list descending:
+#2 
 thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
 thislist.sort(reverse = True)
 print(thislist)
-#3 Sort the list based on how close the number is to 50:
+#3 
 def myfunc(n):
   return abs(n - 50)
 
 thislist = [100, 50, 65, 82, 23]
 thislist.sort(key = myfunc)
 print(thislist)
-# Copy lists
-# Make a copy of a list with the copy() method:
+# 
 thislist = ["apple", "banana", "cherry"]
 mylist = thislist.copy()
 print(mylist)
-#2 also copy
+#2
 thislist = ["apple", "banana", "cherry"]
 mylist = list(thislist)
 print(mylist)
-#3 also
+#3 
 thislist = ["apple", "banana", "cherry"]
 mylist = thislist[:]
 print(mylist)
-#4 Join lists
+#4 
 list1 = ["a", "b", "c"]
 list2 = [1, 2, 3]
 
