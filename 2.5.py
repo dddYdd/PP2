14c889f0de3ea7a1c4f2a96f1e0ff1e9db409bd7
dddYdd <74626121+dddYdd@users.noreply.github.com>
2025-02-08 13:01:46 +0500

    Add files via upload

---
+++ 2.5.py
@@ -0,0 +1,15 @@
+#1 boolean answer
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+#2 Print a message based on whether the condition is True or False:
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+  #3 Check if an object is an integer or not:
+  x = 200
+print(isinstance(x, int))
